bea2d825e9cf4a3e66eed358e2eda4f38e56888a
Pete Saia <pete@lev-interactive.com>
2019-11-20 23:20:05 -0800

    Initial commit

---
+++ submit.js
@@ -0,0 +1,25 @@
+$(function() {
+  // Like it's 2010.
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    var o = {};
+
+    $(this)
+      .serializeArray()
+      .forEach(function(d) {
+        if (o[d.name]) {
+          o[d.name] += ", " + d.value;
+        } else {
+          o[d.name] = d.value;
+        }
+      });
+
+    axios
+      .post(
+        "https://us-central1-stable-259400.cloudfunctions.net/mailinglist",
+        o
+      )
+      .then(function(res) {});
+    $(".fields").html("<p>Your invitation request has been sent.</p>");
+  });
+});
